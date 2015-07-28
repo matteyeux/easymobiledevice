d9b17d954f3ce392fe61ea64cacd9bc4f2abca46
Mathieu Hautebas <mathieu.hautebas2@gmail.com>
2015-07-28 19:43:25 +0200

    Create easymobiledevice.c

---
+++ easymobiledevice.c
@@ -0,0 +1,223 @@
+/*
+*Copyright (c) 2015 Mathieu Hautebas
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int lire_clavier (char *cmd, int taille)
+{	
+  	int i=0;
+    fgets(cmd, taille, stdin);
+   	while(cmd[i] && (cmd[i] != '\n')) i++;
+    cmd[i] = '\0'; 
+    return (i); 
+}
+
+int main(int argc, char const *argv[])
+{	
+	int command;
+	char cmd[30];
+	char UDID[41];
+	int taille;
+
+	system("clear");
+	printf ("Que souhaitez-vous faire \n");
+	printf (" 1) ideviceactivation\n");
+	printf (" 2) idevicebackup\n");
+	printf (" 3) idevicecrashreport\n");
+	printf (" 4) idevicedate\n");
+	printf (" 5) idevicedebug\n");
+	printf (" 6) idevicedebugserverproxy\n");
+	printf (" 7) idevicediagnostics\n");
+	printf (" 8) ideviceenterrecovery\n");
+	printf (" 9) idevice_id \n");
+	printf ("10) ideviceimagemounter\n");
+	printf ("11) ideviceinfo\n");
+	printf ("12) ideviceinstaller\n");
+	printf ("13) idevicename\n");
+	printf ("14) idevicenotificationproxy\n");
+	printf ("15) idevicepair\n");
+	printf ("16) ideviceprovision\n");
+	printf ("17) idevicerestore\n"); 
+	printf ("18) idevicescreenshot\n");
+	printf ("19) idevicesyslog\n");
+	printf ("20) iRecovery\n\n");
+	printf("Commande : ");
+	scanf ("%d", &command);
+	lire_clavier(cmd, sizeof(cmd));
+	
+	//printf("Entrez votre UDID : n");
+	//scanf("%s", UDID);
+
+
+
+	if (command == 23 || !strcmp(cmd, "ideviceactivation"))
+	{
+		printf("OK\n");
+	}
+
+	if (command == 1) //ideviceactivation
+	{	int activator;
+		system ("clear");
+		printf("1) Activer votre appareil\n");
+		printf("2) Desactiver votre appareil\n");
+		scanf ("%d", &activator);
+
+		if (activator == 1)
+		{	
+			printf("Activation\n");
+			system("ideviceactivation activate");
+		}
+		if (activator == 2)
+		{
+			printf("Desactivation\n");
+			system("ideviceactivation deactivate");
+		}
+	}
+
+	if (command == 2) //idevicebackup
+	{	int backup;
+		// Ajouter l'UDID
+		printf("1) Sauvegarder\n");
+		printf("2) Restaurer la sauvegarde\n");
+		scanf ("%d", &backup);
+		if (backup == 1)
+		{
+			printf("Sauvegarde\n");
+			system("idevicebackup backup -d -u ");
+		}
+		if (backup == 2)
+		{
+			printf("Restauration de la sauvegarde\n");
+			//indiquer ou se trouve la sauvegarde
+		}
+	}
+	if (command == 3) //idevicecrashreport
+	{
+		printf("idevicecrashreport\n");
+	}
+	if (command == 4) //idevicedate
+	{
+		printf("idevicedate\n");
+		system("idevicedate");
+	}
+	if (command == 5) //idevicedebug
+	{
+		printf("idevicedebug\n"); //A tester
+	}
+	if (command == 6) //idevicedebugserverproxy
+	{	
+		int port;
+		printf("idevicedebugserverproxy\n");
+		//printf("port : ");
+		//scanf("%d", &port);
+		//printf("%d\n",port );
+		system("idevicedebugserverproxy -d "); /*Voir pour ajouter un port*/ 
+	}
+	if (command == 7)
+	{	
+		printf("idevicediagnostics\n");
+	}
+	if (command == 8) //idevicediagnostics
+	{
+		printf("ideviceenterrecovery\n");
+		system("ideviceenterrecovery -u");
+	}
+	if (command == 9) //idevice_id
+	{
+		printf("idevice_id\n");
+		system("idevice_id -l"); //OK
+	}
+	if (command == 10) //ideviceimagemounter
+	{
+		printf("ideviceimagemounter\n");
+	}
+	if (command == 11) //ideviceinfo
+	{
+		printf("ideviceinfo\n");
+		system("ideviceinfo");
+	}
+	if (command == 12) //ideviceinstaller
+	{
+		printf("ideviceinstaller\n");
+	}
+	if (command == 13) //idevicename
+	{
+		printf("idevicename\n");
+		system("idevicename");
+	}
+	if (command == 14) //idevicenotificationproxy
+	{
+		printf("idevicenotificationproxy\n");
+	}
+	if (command == 15) //idevicepair
+	{	
+		int apairage;
+
+		printf ("1) idevicepair pair\n");
+		printf ("2) idevicepair unpair\n");
+		scanf("%d", &apairage);
+
+		if (apairage == 1)
+		{
+			printf("idevicepair pair\n");
+		}
+		if (apairage == 2)
+		{
+			printf("idevicepair unpair\n");
+		}
+		else {
+			printf("Mauvaise option\n");
+		}
+
+	}
+	if (command == 16) //ideviceprovision
+	{
+		printf("ideviceprovision\n");
+	}
+	if (command == 17) //idevicerestore
+	{	
+
+		printf("idevicerestore\n"); 
+	}
+	if (command == 18) //idevicescreenshot
+	{
+		system("idevicescreenshot");
+	}
+	if (command == 19) //idevicesyslog
+	{
+		printf("idevicesyslog\n");
+		system("idevicesyslog");
+	}
+	if (command == 20) //iRecovery
+	{	
+		int recovery;
+		printf("1) Envoyer un fichier vers l'appareil\n");
+		printf("2) Envoyer un payload grâce à limera1n\n");
+		printf("3) Sortir l'appareil du Recovery Mode\n");
+		printf("4) Executer un script recovery\n");
+		scanf("%d", &recovery);
+		printf("%d\n", recovery);
+
+		if (recovery == 1)
+		{	
+			printf("Selectionner le repertoire vers le fichier à envoyer : ");
+
+		}
+	}
+}
